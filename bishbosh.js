84e04c6f72791d90f8200492043fb75edbeeaaf6
OusmanNdaw <105175511+OusmanNdaw@users.noreply.github.com>
2022-06-28 00:28:44 +0200

    Add files via upload

---
+++ bishbosh.js
@@ -0,0 +1,14 @@
+Number, Bish, Bosh;
+for (number = 0, number >= 100, number++){ 
+        <script> 
+        string number;
+        for(number = 1; number >=100; number++)
+                {
+                if(number % 3 == 0) {alert(Bish)};
+                if(number % 4 == 0) {alert(Bosh)};
+        }
+        </script>  
+}
+
+
+
